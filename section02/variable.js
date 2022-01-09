8f3d46c0ede8c8452719dc68474aad52910e4690
Eumericano <51465561+EumYoungHyun@users.noreply.github.com>
2022-01-09 15:08:06 +0900

    feat: 변수 재 할당과 스코프 비교

--- section02/variable.js
+++ section02/variable.js
@@ -1,15 +1,15 @@
 // var를 지양하자
 
-// console.log(name); // undefined 출력 (에러 X)
+console.log(name); // undefined (에러 X)
 
-// var name = "이름";
-// var name = "이름2";
-// var name = "이름3";
-// var name = "이름3";
-// var name = "이름4";
-// name = "이름5";
+var name = "이름";
+var name = "이름2";
+var name = "이름3";
+var name = "이름3";
+var name = "이름4";
+name = "이름5";
 
-// console.log(name); // 이름 5 출력
+console.log(name); // 이름 5
 
 /**
  * var의 특징
@@ -20,9 +20,9 @@
  * 코드가 확장함에 따라 무결성을 지키기 어려워짐
  */
 
-// let name = "이름";
-// let name = "이름 2";
-// const name = "const 이름";
+let name = "이름";
+let name = "이름 2";
+const name = "const 이름";
 
 /**
  * let과 const는 이런 상황을 미리 방지
@@ -44,6 +44,7 @@ name = "let 이름2";
 console.log(name); // let 이름2
 name = "let 이름3";
 console.log(name); // let 이름3
+
 /**
  * let은 재할당 가능
  */
